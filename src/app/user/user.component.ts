81ac04fbc5b126249141547ad5f0d7f32d7cc394
Daniel Voicu <daniel.voicu@softelligence.net>
2019-11-14 22:51:20 +0200

    unsubscribe on ngOnDestroy refactor code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,36 +1,35 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { TestService } from "../services/test.service";
 import { User } from "../models/user";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-user",
   templateUrl: "./user.component.html",
   styleUrls: ["./user.component.scss"]
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   users: User[];
   users2: User[];
+
+  subscription: Subscription = new Subscription();
 
   constructor(private service: TestService) {}
 
   ngOnInit(): void {
-    this.service.test1().subscribe(
+    this.subscribeToUsers();
+  }
+
+  subscribeToUsers() {
+    const usersSubscription = this.service.users$.subscribe(
       data => {
         console.log("this.service.users$ subscribe");
-
-        this.users2 = data;
-      },
-      error => console.error(error)
-    );
-
-    this.service.users$.subscribe(
-      data => {
-        console.log("this.service.users$ subscribe");
-
         this.users = data;
       },
       error => console.error(error)
     );
+
+    this.subscription.add(usersSubscription);
   }
 
   addItem(): void {
@@ -39,5 +38,9 @@ export class UserComponent implements OnInit {
 
   loadAll(): void {
     this.service.loadAll();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
