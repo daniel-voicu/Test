1b92fcfeea6c0c05f34884f7d6d3bdcf84a71353
Daniel Voicu <daniel.voicu@softelligence.net>
2019-11-14 22:50:07 +0200

    emit values only when data != null refactor code

--- src/app/services/test.service.ts
+++ src/app/services/test.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
 import { User } from "../models/user";
 import { HttpClient } from "@angular/common/http";
-import { tap, map, catchError, shareReplay, share } from "rxjs/operators";
+import { filter, distinctUntilChanged } from "rxjs/operators";
 
 const usersUrl = "https://angular-material-api.azurewebsites.net/users";
 
@@ -12,50 +12,21 @@ const usersUrl = "https://angular-material-api.azurewebsites.net/users";
 export class TestService {
   constructor(private httpClient: HttpClient) {}
 
-  private cache = new BehaviorSubject<User[]>([]);
-  private cache2: Observable<User[]>;
+  private cache = new BehaviorSubject<User[]>(null);
 
   get users$(): Observable<User[]> {
-    if (this.cache.getValue().length === 0) {
-      return this.loadAll();
+    if (!this.cache.getValue()) {
+      this.loadAll();
     }
-    console.log("users$ getter");
-    return this.cache.asObservable();
+    return this.cache.asObservable().pipe(filter(users => users !== null));
   }
 
-  loadAll(): Observable<User[]> {
+  loadAll() {
     const usersUrl = "https://angular-material-api.azurewebsites.net/users";
 
-    return this.httpClient.get<User[]>(usersUrl).pipe(
-      tap(data => {
-        let user = new User();
-        user.name = "Daniel Voicu";
-
-        data.push(user);
-
-        this.cache.next(data);
-      })
-    );
-  }
-
-  test1(): Observable<User[]> {
-    if (!this.cache2) {
-      this.cache2 = new Observable<User[]>(observer => {
-        this.httpClient.get<User[]>(usersUrl).subscribe(
-          value => {
-            observer.next(value);
-          },
-          () => {
-            observer.error("Error loading @self user");
-          },
-          () => {
-            observer.complete();
-          }
-        );
-      }).pipe(shareReplay(1));
-    }
-
-    return this.cache2;
+    this.httpClient.get<User[]>(usersUrl).subscribe(data => {
+      this.cache.next(data);
+    });
   }
 
   add(): void {
@@ -63,7 +34,6 @@ export class TestService {
     user.name = `Test ${Date.now()}`;
 
     this.addToCache(user);
-    this.addToCache2(user);
   }
 
   addToCache(user: User): void {
@@ -71,12 +41,5 @@ export class TestService {
     users.push(user);
 
     this.cache.next(users);
-  }
-
-  addToCache2(user: User): void {
-    this.cache2.pipe(
-      map(users => users.push(user)),
-      shareReplay()
-    );
   }
 }
